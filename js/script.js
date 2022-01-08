671f361a7d5fdfbc6e9d1a7387d9b3e9f7905ce7
Isaac Cruz <cruzisaac51@gmail.com>
2022-01-08 17:54:39 -0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const d = new Date();
+const year = d.getFullYear();
+const fulldate = `${year}`;
+document.querySelector("#dateyear").textContent = fulldate;
+
+// const day = d.getDay();
+// const month = d.getMonth();
+// const fdate = `${day}/${month}/${year}`;
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+const fdate = d.toLocaleDateString("en-UK", options);
+document.querySelector("#lastUpdated").textContent = fdate;
+//     let oLastModif = new Date(document.lastModified)
+// document.querySelector("#lastUpdated").textContent = fdate;
+
+
+function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+
+WebFont.load({
+    google: {
+        families: [
+            'krub'
+        ]
+    }
+});
