3e86262a659ba0bda397d9b0ed6234a8843c132a
Isaac Cruz <cruzisaac51@gmail.com>
2022-01-08 21:49:11 -0600

    portal assingament wdd330

--- js/script.js
+++ js/script.js
@@ -2,25 +2,49 @@ const d = new Date();
 const year = d.getFullYear();
 const fulldate = `${year}`;
 document.querySelector("#dateyear").textContent = fulldate;
-
-// const day = d.getDay();
-// const month = d.getMonth();
-// const fdate = `${day}/${month}/${year}`;
 const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 const fdate = d.toLocaleDateString("en-UK", options);
 document.querySelector("#lastUpdated").textContent = fdate;
-//     let oLastModif = new Date(document.lastModified)
-// document.querySelector("#lastUpdated").textContent = fdate;
 
 
 function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
 
-WebFont.load({
-    google: {
-        families: [
-            'krub'
-        ]
+
+// links for weekly homework
+const homeworkjson = "json/weeks.json";
+
+const hmapi = async(key, value) => {
+    try {
+        const response = await fetch(`${homeworkjson}`);
+        const response2 = await response.json();
+        let listofweeks = response2[key];
+        listofweeks.forEach((element) => {
+            if (element.name === value) {
+                //create elements
+                const createCards = document.createElement("div");
+                let cardinfo = document.createElement("div");
+                let titleh2 = document.createElement("h2");
+                let information = document.createElement("a");
+
+                //set attributes
+                createCards.setAttribute("class", "card");
+                information.setAttribute("class", "motto-card");
+                information.setAttribute("href", element.link);
+
+                //insert elements
+                titleh2.innerHTML = element.name;
+                information.innerHTML = element.link;
+                cardinfo.appendChild(titleh2);
+                cardinfo.appendChild(information);
+                createCards.appendChild(cardinfo);
+                document.querySelector("section.the-section").appendChild(createCards);
+            }
+        });
+        return listofweeks;
+    } catch (error) {
+      console.log(error);
     }
-});
+};
+hmapi("homework", "week1");
